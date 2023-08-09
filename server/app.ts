0d1a9e353c63dc9a5af92ad96aedfb2779cf0e3f
Taherpatrawala <109360109+Taherpatrawala@users.noreply.github.com>
2023-08-09 08:56:48 +0530

    app setup

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,16 @@
-const express = require("express");
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
+dotenv.config();
+app.use(cors());
+app.use(express.json());
 
-app.listen(3000, () => {});
+mongoose.connect(process.env.MONGO_URI as string).then(() => {
+  const port = 8080;
+  app.listen(port, () => {
+    console.log(`Server started at port ${port}`);
+  });
+});
