3b41844da1bbd92197956c3662ced26b10a801a9
Taherpatrawala <109360109+Taherpatrawala@users.noreply.github.com>
2023-08-12 10:00:53 +0530

    import socket.io

--- server/app.ts
+++ server/app.ts
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import http from "http";
+import io from "socket.io";
 
 const app = express();
 dotenv.config();
