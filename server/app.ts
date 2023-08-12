b604c33d259848e934b0ed544266cb845acec58b
Taherpatrawala <109360109+Taherpatrawala@users.noreply.github.com>
2023-08-12 11:54:10 +0530

    Initialized socket.io in app.ts

--- server/app.ts
+++ server/app.ts
@@ -3,12 +3,20 @@ import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import http from "http";
-import io from "socket.io";
+import { Server } from "socket.io";
 
 const app = express();
 dotenv.config();
 app.use(cors());
 app.use(express.json());
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
 
 mongoose.connect(process.env.MONGO_URI as string).then(() => {
   const port = 8080;
