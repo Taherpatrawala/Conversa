8d8c62b500a7e5ec061cf5f12bc1aacac1523660
Taherpatrawala <109360109+Taherpatrawala@users.noreply.github.com>
2023-08-28 16:57:53 +0530

    made cookie route secure

--- server/routes/googleRoutes.ts
+++ server/routes/googleRoutes.ts
@@ -14,7 +14,7 @@ googleRoutes.use(
     store: MongoStore.create({
       mongoUrl: `${process.env.MONGO_SECRET_URI}`,
     }),
-    cookie: { maxAge: 60000000000 },
+    cookie: { maxAge: 60000000000, secure: true },
   })
 );
 googleRoutes.use(passport.initialize());
